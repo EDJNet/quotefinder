2ad04a9580efc335136704423639918877945d43
Giorgio Comai <g@giorgiocomai.eu>
2019-06-19 15:58:39 +0200

    basic app with placeholders

--- R/app_ui.R
+++ R/app_ui.R
@@ -4,8 +4,36 @@ app_ui <- function() {
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # List the first level UI elements here 
-    fluidPage(
-      h1("edjnetquotefinder")
+    shinydashboard::dashboardPage(
+      header = shinydashboard::dashboardHeader(title = "EdjNet's QuoteFinder"),
+      sidebar = shinydashboard::dashboardSidebar(
+        shinydashboard::sidebarMenu(
+          shinydashboard::menuItem(text = "MEPs on Twitter",
+                                   tabName = "tab_twitter_mep",
+                                   icon = shiny::icon("twitter")),
+          shinydashboard::menuItem(text = "Quotes from EU institutions",
+                                   tabName = "tab_eu_official",
+                                   icon = shiny::icon("globe"), badgeLabel = "new"),
+          disable = FALSE,
+          id = "tabs"
+        )
+      ),
+      body = shinydashboard::dashboardBody(
+        
+        shinydashboard::tabItems(
+          
+          ##### TwitterMEP tab ####
+          shinydashboard::tabItem(tabName = "tab_twitter_mep", 
+                                  shiny::h3("placeholder")
+          ),
+          ##### TwitterMEP tab ####
+          shinydashboard::tabItem(tabName = "tab_eu_official", 
+                                  shiny::h3("placeholder")
+          )
+          
+        )
+      ), title = "EDJNet's QuoteFinder",
+      skin = "purple"
     )
   )
 }
@@ -16,13 +44,13 @@ golem_add_external_resources <- function(){
   addResourcePath(
     'www', system.file('app/www', package = 'edjnetquotefinder')
   )
- 
+  
   tags$head(
-    golem::js(),
-    golem::favicon()
+    golem::activate_js(),
+    golem::favicon(),
     # Add here all the external resources
     # If you have a custom.css in the inst/app/www
     # Or for example, you can add shinyalert::useShinyalert() here
-    #tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+    tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
   )
 }
