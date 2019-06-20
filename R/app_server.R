c21b6d6f93e968b0f3559e0afef950ae71ee901f
Giorgio Comai <giorgiocomai@gmail.com>
2019-06-20 15:06:05 +0200

    basic components of shiny app taken from castarter

--- R/app_server.R
+++ R/app_server.R
@@ -1,4 +1,78 @@
 #' @import shiny
 app_server <- function(input, output,session) {
+  output$letters <- renderText(paste(head(tweets$screen_name), collapse = ", "))
+  output$bla <- shiny::renderText("bla")
+  
+  
+  tsGG <- shiny::eventReactive(input$go, {
+    if (input$freq=="Absolute frequency") {
+      castarter::ShowAbsoluteTS(terms = as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))),
+                                dataset = dataset,
+                                type = "graph",
+                                rollingAverage = input$rollingAverage,
+                                startDate = input$dateRange[1],
+                                endDate = input$dateRange[2])
+      
+    } else if (input$freq=="Relative frequency") {
+      castarter::ShowRelativeTS(terms = as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))),
+                                dataset = dataset,
+                                type = "graph",
+                                rollingAverage = input$rollingAverage,
+                                startDate = input$dateRange[1],
+                                endDate = input$dateRange[2])
+    }
+  })
+  
+  output$freqPlot <- shiny::renderPlot({
+    if (input$go==0) {
+      
+    } else {
+      tsGG()
+    }
+  })
+  
+  kwic_react <- shiny::eventReactive(input$go, {
+    temp <- dataset %>%
+      dplyr::filter(date>input$dateRange[1], date<input$dateRange[2]) %>%
+      dplyr::filter(stringr::str_detect(string = sentence,
+                                        pattern = stringr::regex(ignore_case = TRUE,
+                                                                 pattern = paste(as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))), collapse = "|")))) %>%
+      dplyr::mutate(Source = paste0("<a target='_blank' href='", link, "'>", title, "</a><br />")) %>%
+      dplyr::rename(Sentence = sentence, Date = date) %>%
+      dplyr::select(Date, Source, Sentence) %>%
+      dplyr::arrange(desc(Date))
+    
+    if (length(as.character(tolower(trimws(stringr::str_split(string = input$term, pattern = ",", simplify = TRUE)))))==1) {
+      temp$Sentence <- purrr::map_chr(.x = temp$Sentence,
+                                      .f = function (x)
+                                        paste(c(rbind(as.character(stringr::str_split(string = x,
+                                                                                      pattern = stringr::regex(pattern = as.character(input$term), ignore_case = TRUE), simplify = TRUE)),
+                                                      c(paste0("<span style='background-color: #FFFF00'>",
+                                                               as.character(stringr::str_extract_all(string = x,
+                                                                                                     pattern = stringr::regex(as.character(input$term),
+                                                                                                                              ignore_case = TRUE),
+                                                                                                     simplify = TRUE)),
+                                                               "</span>"), ""))),
+                                              collapse = ""))
+    }
+    temp
+  })
+  
+  # Renders table at the bottom of the main tab
+  output$kwic <- DT::renderDataTable(expr = kwic_react(),
+                                     server = TRUE,
+                  options = list(pageLength = 3,
+                                 lengthMenu = c(3, 5, 10, 15, 20)),
+                  escape = FALSE,
+                  rownames = FALSE)
+  
+  output$dateRangeInput_UI <- renderUI({shiny::dateRangeInput(inputId = "dateRange",
+                                                              label = "Date range",
+                                                              start = min(dataset$date),
+                                                              end = max(dataset$date),
+                                                              weekstart = 1)})
+
+  
+  #  tweets_r <- callModule(mod_qf_show_tweets_server, "qf_show_tweets_ui_1", tweets_r)
   # List the first level callModules here
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -5,7 +5,7 @@ app_ui <- function() {
     golem_add_external_resources(),
     # List the first level UI elements here 
     shinydashboard::dashboardPage(
-      header = shinydashboard::dashboardHeader(title = "EdjNet's QuoteFinder"),
+      header = shinydashboard::dashboardHeader(title = tags$a(href='https://www.europeandatajournalism.eu/',                                                              tags$img(src='logo_50_230_w.png'))), # for animated version of the logo, check https://stackoverflow.com/questions/31440564/adding-a-company-logo-to-shinydashboard-header
       sidebar = shinydashboard::dashboardSidebar(
         shinydashboard::sidebarMenu(
           shinydashboard::menuItem(text = "MEPs on Twitter",
@@ -22,13 +22,30 @@ app_ui <- function() {
         
         shinydashboard::tabItems(
           
-          ##### TwitterMEP tab ####
+          ##### tab_twitter_mep ####
           shinydashboard::tabItem(tabName = "tab_twitter_mep", 
-                                  shiny::h3("placeholder")
+                                  shiny::h3("Twitter MEP"),
+                                  mod_qf_show_tweets_ui("qf_show_tweets_ui_1", tweets_r)
           ),
-          ##### TwitterMEP tab ####
-          shinydashboard::tabItem(tabName = "tab_eu_official", 
-                                  shiny::h3("placeholder")
+          ##### tab_eu_official ####
+          shinydashboard::tabItem(tabName = "tab_eu_official",
+                                  shiny::textInput(inputId = 'term',
+                                                   label = 'Search',
+                                                   value = ""),
+                                  shiny::radioButtons(inputId = "freq",
+                                                      label = NULL,
+                                                      choices = c("Absolute frequency",
+                                                                  "Relative frequency")),
+                                  shiny::sliderInput(inputId = "rollingAverage",
+                                                     label = "Apply rolling average for ... days",
+                                                     min = 1,
+                                                     max = 91,
+                                                     value = 91,
+                                                     round = TRUE),
+                                  shiny::uiOutput(outputId = "dateRangeInput_UI"),
+                                  shiny::actionButton("go", "Go!"),
+
+                                  shiny::fluidRow(DT::dataTableOutput(outputId = "kwic"))
           )
           
         )
@@ -44,7 +61,7 @@ golem_add_external_resources <- function(){
   addResourcePath(
     'www', system.file('app/www', package = 'edjnetquotefinder')
   )
-  
+
   tags$head(
     golem::activate_js(),
     golem::favicon(),
