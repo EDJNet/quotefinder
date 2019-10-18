5ed3a7c302b19526bf1ccc49beec9b25cc63113a
Giorgio Comai <giorgiocomai@gmail.com>
2019-10-18 13:56:21 +0200

    highlight two main steps of the process

--- inst/qf_get_tweets/qf_data/qf_get_tweets.R
+++ inst/qf_get_tweets/qf_data/qf_get_tweets.R
@@ -1,1 +1,4 @@
-rmarkdown::render(input = "/home/qf/qf_data/qf_get_tweets.Rmd")
+# get tweets
+rmarkdown::render(input = "qf_data/qf_get_tweets.Rmd")
+# pre-process tweets
+rmarkdown::render(input = "qf_data/qf_pre_process_tweets.Rmd")
