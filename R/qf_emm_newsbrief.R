b3d10204a00ecf4646af7536fdbb2e21cfa3c951
Giorgio Comai <g@giorgiocomai.eu>
2019-12-09 15:09:37 +0100

    enable explicit category in emm_newsbrief

--- R/qf_emm_newsbrief.R
+++ R/qf_emm_newsbrief.R
@@ -2,7 +2,8 @@
 #' 
 #' Imports and cleans RSS files from [http://emm.newsbrief.eu/]
 #'
-#' @param languages A character vector of one or more languages as two letter codes (e.g. "en").
+#' @param languages A character vector of one or more languages as two letter codes (e.g. "en"). "all", the dafault, includes all available languages. Current available languages are: `c("ar","bg","cs","da","de","el","en","es", "et","fi","fr","hr", "hu","it", "lt","lv","nl","pl","pt","ro","ru", "sk","sl","sv","sw","tr","zh")`
+#' @param category_id A character vector of length 1, defaults to "rtn" (default category with all contents). The id for separate categories can be derived from the URL of the given section, e.g. for EC news the id is "ECnews".
 #' @param shuffle Logical, defaults to TRUE. If TRUE, order in which languages are processed is randomised.
 #' @param keep_xml Logical, defaults to FALSE. If TRUE, removes the xml file downloaded from EMM newsbrief.
 #' @return Nothing, used for its side effects. 
@@ -10,7 +11,8 @@
 #' 
 #' @export
 
-qf_get_emm_newsbrief <- function(languages = c("ar","bg","cs","da","de","el","en","es", "et","fi","fr","hr", "hu","it", "lt","lv","nl","pl","pt","ro","ru", "sk","sl","sv","sw","tr","zh"),
+qf_get_emm_newsbrief <- function(languages = "all",
+                                 category_id = "rtn",
                                  keep_xml = TRUE,
                                  shuffle = TRUE) {
   
@@ -24,22 +26,32 @@ qf_get_emm_newsbrief <- function(languages = c("ar","bg","cs","da","de","el","en
                           i,
                           as.character(Sys.Date()))
     fs::dir_create(path = base_path, recurse = TRUE)
+
     
-    xml_location <- fs::path(base_path,
-                             paste0(Sys.time(), "-emm_rtn_", i, ".xml"))
-    
-    download.file(url = paste0("http://emm.newsbrief.eu/rss/rss?type=rtn&language=", i, "&duplicates=false"),
-                  destfile = xml_location)
+    if (category_id == "rtn") {
+      xml_location <- fs::path(base_path,
+                               paste0(Sys.time(), "-emm_rtn_", i, ".xml"))
+      
+      download.file(url = paste0("http://emm.newsbrief.eu/rss/rss?type=rtn&language=", i, "&duplicates=false"),
+                    destfile = xml_location)
+      
+    } else {
+      xml_location <- fs::path(base_path,
+                               paste0(Sys.time(), "-", category_id, "-", i, ".xml"))
+      download.file(url = paste0("https://emm.newsbrief.eu/rss/rss?type=category&id=",category_id, "&language=", i, "&duplicates=false"),
+                    destfile = xml_location)
+    }
+
     
     xml2_list <- xml2::read_xml(xml_location) %>%
       xml2::xml_find_all(xpath = "//item") %>%
       xml2::as_list()
     
-    
     nrows_rss <- length(xml2_list)
     
     if (nrows_rss>0) {
       rss_df <- tibble::tibble(title = character(nrows_rss),
+                               language = character(nrows_rss),
                                link = character(nrows_rss),
                                description = character(nrows_rss),
                                pubDate = as.POSIXct(x = rep(x = NA, nrows_rss)),
@@ -50,6 +62,7 @@ qf_get_emm_newsbrief <- function(languages = c("ar","bg","cs","da","de","el","en
         temp <- xml2_list %>% magrittr::extract2(i)
         
         rss_df$title[i] <- temp$title %>% as.character()
+        rss_df$language[i] <- temp$language %>% as.character()
         rss_df$link[i] <- temp$link %>% as.character()
         rss_df$description[i] <- temp$description %>% as.character()
         rss_df$pubDate[i] <- temp$pubDate %>% as.character() %>% base::strptime("%a, %d %b %Y %H:%M:%S %z")
@@ -62,7 +75,8 @@ qf_get_emm_newsbrief <- function(languages = c("ar","bg","cs","da","de","el","en
       if (keep_xml == FALSE) {
         fs::file_delete(path = xml_location)
       }
-      saveRDS(object = rss_df, file = xml_location %>%
+      saveRDS(object = rss_df,
+              file = xml_location %>%
                 stringr::str_replace(pattern = stringr::fixed(".xml"),
                                      replacement = ".rds"))
       
@@ -198,7 +212,9 @@ qf_emm_extract_entities <- function(language = NULL,
   }
   
   
-  all_files <- fs::dir_ls(path = fs::path(fs::path("emm_newsbrief", lang_date_combo$language, lang_date_combo$date)),
+  all_files <- fs::dir_ls(path = fs::path("emm_newsbrief",
+                                          lang_date_combo$language,
+                                          lang_date_combo$date),
                           recurse = FALSE,
                           type = "file",
                           glob = "*.rds")
@@ -207,7 +223,7 @@ qf_emm_extract_entities <- function(language = NULL,
   all_feeds <- purrr::map_dfr(.x = all_files,
                               .f = function(x) {
                                 readr::read_rds(x) %>% 
-                                  dplyr::select(pubDate, entity, link)
+                                  dplyr::select(pubDate, entity, link, language)
                                 },
                               .id = "source") %>% 
     dplyr::distinct(link, .keep_all = TRUE) %>% 
@@ -215,7 +231,7 @@ qf_emm_extract_entities <- function(language = NULL,
   
   entities <- all_feeds %>%
     dplyr::transmute(date=as.Date(pubDate),
-                     language=fs::path_dir(all_feeds$source) %>% fs::path_dir() %>% fs::path_file(),
+                     language,
                      entity) %>% 
     tidyr::unnest(cols = entity) %>% 
     dplyr::group_by(date, language, id, name) %>% 
