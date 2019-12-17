765447520a13dc8efa570a73bc782261e37c1c14
Giorgio Comai <giorgiocomai@gmail.com>
2019-12-17 16:50:44 +0100

    fix emm_df not visible in app when run from docker (?)

--- R/run_app.R
+++ R/run_app.R
@@ -6,6 +6,10 @@ qf_dashboard <- function(path) {
   # https://github.com/rstudio/shiny/issues/440
   .GlobalEnv$.quotefinder.path <- path
   on.exit(rm(.quotefinder.path, envir=.GlobalEnv))
+  
+  emm_df <- readRDS(file = file.path(path,"qf_data", "emm_newsbrief_processed", "emm_newsbrief_processed.rds"))
+  emm_languages <- readRDS(file = file.path(path,"qf_data", "emm_newsbrief_processed", "emm_newsbrief_languages.rds"))
+  
   shiny::runApp(appDir = system.file("app", package = "quotefinder"))
 }
   
