a0402ce6f9dadde6d4a77221aeab7ffe93748c79
Giorgio Comai <g@giorgiocomai.eu>
2019-12-20 14:59:00 +0100

    minor adjustments to loaded packages in various docker images

--- inst/app/global.R
+++ inst/app/global.R
@@ -26,7 +26,6 @@ library("shinybusy")
 
 extrafont::loadfonts(quiet = TRUE)
 
-print("Loading emm_df")
 emm_df <- readRDS(file = file.path(.quotefinder.path,"qf_data", "emm_newsbrief_processed", "emm_newsbrief_processed.rds"))
 emm_languages <- readRDS(file = file.path(.quotefinder.path,"qf_data", "emm_newsbrief_processed", "emm_newsbrief_languages.rds"))
 
